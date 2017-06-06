d992657851f16305d5b7cb9e86fec4f257c14840
meatball <mb0520@sinyi-tech.com>
2017-06-06 09:51:00 +0800

    top_banner 版型

--- src/components/layout/unify.vue
+++ src/components/layout/unify.vue
@@ -1,21 +1,53 @@
 <template>
   <div>
     <div class="top_banner">
-
+      <nav layout="row center">
+        <a class="logo" self="center"><img
+          src="https://htmlstream.com/preview/unify-v1.9.8/assets/img/themes/logo1-default.png"></a>
+        <ul layout="row center-stretch">
+          <li>HOME</li>
+          <li>PAGES</li>
+          <li>BLOG</li>
+          <li>PORTFOLIO</li>
+          <li>FEATRUES</li>
+          <li>SHORTCODES</li>
+          <li>DEMO</li>
+        </ul>
+        <div class="icons" self="center" layout="row">
+          <span class="icon-cart counter"></span>
+          <span class="icon-search"></span>
+        </div>
+      </nav>
+      <div class="bottom" layout="row-center-center">
+        <div class="content" layout="column-center-center">
+          <div class="title">UNIFY BOOTSTRAP TEMPLATE</div>
+          <h1>WE ARE CREATIVE COMPANY</h1>
+          <div class="buttons">
+            <span><a href="">Learn More</a></span>
+            <span><a href="">Purchase Now</a></span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-//  import $ from 'jquery'
-
-
+  import icon from '../../assets/icon.css'
+  let set_top_banner_h = ()=> {
+    const h = $(window).height()
+    console.log(h)
+    $(".top_banner").css("height", h)
+  }
 
   export default {
-    mounted:()=>{
-      let a = Array.of("test","ok")
-      console.log(a)
-      console.log($(".top_banner"))
+    mounted() {
+      set_top_banner_h()
+      $(window).resize(res=> {
+        set_top_banner_h()
+
+
+      })
     }
   }
 
